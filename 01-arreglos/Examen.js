53ba9b89642c855cc9b465a56b720ba62debd06f
MiguelAlvarez97 <miguel9730@hotmail.com>
2018-12-03 10:26:42 -0500

    com

--- 01-arreglos/Examen.js
+++ 01-arreglos/Examen.js
@@ -6,60 +6,76 @@ var fs = require("fs");
 var rxjs = require('rxjs');
 var mergemap = require('rxjs/operators').mergemap;
 var map = require('rxjs/operators').map;
-function main() {
-    console.log('1) Busque los tipos de "gender" en el arreglo `people.json`');
-    inicializarBase();
-    console.log('2) Busque los tipos de "eye_color" en el arreglo `people.json`');
+var peopleJson = 'people.json';
+function leerBDD(nombreArchivo) {
+    return new es6_promise_1.Promise(function (resolve, reject) {
+        fs.readFile(nombreArchivo, 'utf-8', function (error, content) {
+            if (error) {
+                reject('\n\tError al leer el archivo JSON');
+            }
+            else {
+                resolve(content);
+            }
+        });
+    });
 }
-main();
-/*function leerArchivoJSON(){
-    const leerBDD$ = rxjs.from(leerBDD());
+function consultasDatos() {
+    return new es6_promise_1.Promise(function (resolve) {
+        leerBDD(peopleJson)
+            .then(function (content) {
+            var respuesta = content;
+            resolve(respuesta);
+        })["catch"](function (resultadoError) {
+            console.log('Algo malo paso', resultadoError);
+        });
+    });
+}
+consultasDatos().then(function (cont) {
+    console.log('\n1) Busque los tipos de "gender" en el arreglo `people.json`');
+    var datosJSON = JSON.parse(cont.toString());
+    datosJSON.map(function (valor) {
+        return valor.gender;
+    }).forEach(function (elementoLeido) { return console.log(elementoLeido); });
+});
+consultasDatos().then(function (cont) {
+    console.log('\n2) Busque los tipos de "eye_color" en el arreglo `people.json`');
+    var datosJSON = JSON.parse(cont.toString());
+    datosJSON.map(function (valor) {
+        return valor.eye_color;
+    }).forEach(function (elementoLeido) { return console.log(elementoLeido); });
+});
+consultasDatos().then(function (cont) {
+    console.log('\n3) Busque los tipos de "skin_color" en el arreglo `people.json`');
+    var datosJSON = JSON.parse(cont);
+    datosJSON.map(function (valor) {
+        return valor.skin_color;
+    }).forEach(function (elementoLeido) { return console.log(elementoLeido); });
+});
+consultasDatos().then(function (cont) {
+    console.log('\n4) Busque los tipos de "hair_color" en el arreglo `people.json`');
+    var datosJSON = JSON.parse(cont);
+    datosJSON.map(function (valor) {
+        return valor.hair_color;
+    }).forEach(function (elementoLeido) { return console.log(elementoLeido); });
+});
+function inicializarBase() {
+    /*const leerBDD$ = rxjs.from(leerBDD());
 
     return leerBDD$
         .pipe(
-            mergeMap(
+            map(
                 (respuestaLeerBDD: RespuestaBDD) => {
                     if (respuestaLeerBDD.bdd) {
                         // truty / {}
                         return rxjs.of(respuestaLeerBDD)
                     } else {
                         // falsy / null
-                        return rxjs.from(crearBDD())
+                        //return rxjs.from(crearBDD())
+                        console.log('Error al leer la base de datos')
+
                     }
                 }
             )
         );
-}*/
-function inicializarBase() {
-    var leerBDD$ = rxjs.from(leerBDD());
-    return leerBDD$
-        .pipe(mergemap(function (respuestaLeerBDD) {
-        if (respuestaLeerBDD.bdd) {
-            // truty / {}
-            return rxjs.of(respuestaLeerBDD);
-        }
-        else {
-            // falsy / null
-            //return rxjs.from(crearBDD())
-            console.log('Error al leer la base de datos');
-        }
-    }));
-}
-function leerBDD() {
-    return new es6_promise_1.Promise(function (resolve) {
-        fs.readFile('people.json', 'utf-8', function (error, contenidoLeido) {
-            if (error) {
-                resolve({
-                    mensaje: 'Base de datos vacia',
-                    bdd: null
-                });
-            }
-            else {
-                resolve({
-                    mensaje: 'Lectura Correcta...',
-                    bdd: contenidoLeido
-                });
-            }
-        });
-    });
+*/
 }
--- 01-arreglos/Examen.ts
+++ 01-arreglos/Examen.ts
@@ -4,40 +4,97 @@ const fs = require("fs");
 const rxjs = require('rxjs')
 const mergemap = require('rxjs/operators').mergemap;
 const map = require('rxjs/operators').map;
+const peopleJson = 'people.json';
 
-
-
-
-function main(){
-    console.log('1) Busque los tipos de "gender" en el arreglo `people.json`')
-
-    inicializarBase().pipe();
-    console.log('2) Busque los tipos de "eye_color" en el arreglo `people.json`')
+function leerBDD(nombreArchivo) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(nombreArchivo, 'utf-8', function (error, content) {
+            if (error) {
+                reject('\n\tError al leer el archivo JSON');
+            }
+            else {
+                resolve(content);
+            }
+        });
+    });
 }
 
-main()
-/*function leerArchivoJSON(){
-    const leerBDD$ = rxjs.from(leerBDD());
+function consultasDatos() {
+    return new Promise(function (resolve) {
+        leerBDD(peopleJson)
+            .then(function (content) {
+                const respuesta = content;
+                resolve(respuesta);
+            })
+            .catch(function (resultadoError) {
+                console.log('Algo malo paso', resultadoError);
+            });
+    });
+}
 
-    return leerBDD$
-        .pipe(
-            mergeMap(
-                (respuestaLeerBDD: RespuestaBDD) => {
-                    if (respuestaLeerBDD.bdd) {
-                        // truty / {}
-                        return rxjs.of(respuestaLeerBDD)
-                    } else {
-                        // falsy / null
-                        return rxjs.from(crearBDD())
-                    }
-                }
-            )
+consultasDatos().then(
+    function (cont) {
+        console.log('\n1) Busque los tipos de "gender" en el arreglo `people.json`');
+        const datosJSON = JSON.parse(cont.toString());
+        datosJSON.map(
+            (valor)=>{
+                return valor.gender;
+            }
+        ).forEach(
+            (elementoLeido) => console.log(elementoLeido)
         );
-}*/
+    }
+)
+
+
+consultasDatos().then(
+    function (cont) {
+        console.log('\n2) Busque los tipos de "eye_color" en el arreglo `people.json`');
+        const datosJSON = JSON.parse(cont.toString());
+        datosJSON.map(
+            (valor)=>{
+                return valor.eye_color;
+            }
+        ).forEach(
+            (elementoLeido) => console.log(elementoLeido)
+        );
+    }
+)
+
+consultasDatos().then(
+    function (cont) {
+        console.log('\n3) Busque los tipos de "skin_color" en el arreglo `people.json`');
+        const datosJSON = JSON.parse(cont.toString());
+        datosJSON.map(
+            (valor)=>{
+                return valor.skin_color;
+            }
+        ).forEach(
+            (elementoLeido) => console.log(elementoLeido)
+        );
+    }
+)
+
+consultasDatos().then(
+    function (cont) {
+        console.log('\n4) Busque los tipos de "hair_color" en el arreglo `people.json`');
+        const datosJSON = JSON.parse(cont.toString());
+        datosJSON.map(
+            (valor)=>{
+                return valor.hair_color;
+            }
+        ).forEach(
+            (elementoLeido) => console.log(elementoLeido)
+        );
+    }
+)
+
+
+
 
 function inicializarBase() {
 
-    const leerBDD$ = rxjs.from(leerBDD());
+    /*const leerBDD$ = rxjs.from(leerBDD());
 
     return leerBDD$
         .pipe(
@@ -55,40 +112,14 @@ function inicializarBase() {
                 }
             )
         );
-
+*/
 }
 
 
-function leerBDD(){
-    return new Promise(
-        (resolve) => {
-            fs.readFile(
-                'people.json',
-                'utf-8',
-                (error, contenidoLeido) => {
-                    if (error) {
-                        resolve({
-                            mensaje: 'Base de datos vacia',
-                            bdd: null
 
-                        });
-                    } else {
-                        resolve({
-                            mensaje: 'Lectura Correcta...',
-                            bdd: contenidoLeido,
-
-
-                        });
-                    }
-
-                }
-            );
-        }
-
-    );
-}
 
 interface RespuestaBDD {
     mensaje: string,
     bdd: string
 }
+
